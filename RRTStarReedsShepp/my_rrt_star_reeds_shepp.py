762e9cb5732e89ff6dd130a435958e7ebd75a656
ligongh <hanligong@gmail.com>
2018-11-15 00:30:47 -0500

    sample control

--- RRTStarReedsShepp/my_rrt_star_reeds_shepp.py
+++ RRTStarReedsShepp/my_rrt_star_reeds_shepp.py
@@ -28,7 +28,7 @@ class RRT():
 
     def __init__(self, start, goal, obstacleList, randArea,
                  goalSampleRate=10, maxIter=400, star=True,
-                 curvature=1, step_size=0.1):
+                 curvature=1, step_size=0.1, sample_control=False):
         """
         Setting Parameter
 
@@ -48,6 +48,7 @@ class RRT():
         self.star = star
         self.curvature = curvature
         self.step_size = step_size
+        self.sample_control = sample_control
 
     def Planning(self, animation=True):
         """
@@ -59,6 +60,10 @@ class RRT():
         self.nodeList = [self.start]
         for i in range(self.maxIter):
             rnd = self.get_random_point()
+            if self.sample_control:
+                # sample a new rnd
+                nind = self.GetNearestListIndex(self.nodeList, rnd)
+                rnd = self.sample_via_control(self.nodeList[nind])
             nind = self.GetNearestListIndex(self.nodeList, rnd)
 
             newNode = self.steer(rnd, nind)
@@ -83,6 +88,32 @@ class RRT():
             return None
         path = self.gen_final_course(lastIndex)
         return path
+    
+    def sample_via_control(self, node):
+        kappa = random.uniform(-self.curvature, self.curvature)
+        arclen = random.random() * 1
+        x, y, yaw = node.x, node.y, node.yaw
+        if abs(kappa) < 0.001:
+            # straight line
+            nx = x + arclen*math.cos(yaw)
+            ny = y + arclen*math.sin(yaw)
+        else:
+            R = 1 / abs(kappa)
+            angle = arclen / R
+            da = 0
+            if kappa < 0:
+                ox = x + R*math.cos(yaw-math.pi/2)
+                oy = y + R*math.sin(yaw-math.pi/2)
+                a = math.pi + (yaw - math.pi/2) - angle
+                da = -angle
+            else:
+                ox = x + R*math.cos(yaw+math.pi/2)
+                oy = y + R*math.sin(yaw+math.pi/2)
+                a = math.pi - (yaw + math.pi/2) + angle
+                da = angle
+            nx = ox + R*math.cos(a)
+            ny = oy + R*math.sin(a)
+        return Node(nx, ny, self.pi_2_pi(yaw+da))
 
     def choose_parent(self, newNode, nearinds):
         if len(nearinds) == 0:
@@ -318,7 +349,7 @@ def main(opt):
 
     rrt = RRT(start, goal, randArea=[-2.0, 15.0], obstacleList=obstacleList,
               goalSampleRate=opt.goal_sample_rate, star=not opt.no_star,
-              curvature=opt.curvature, step_size=opt.step_size)
+              curvature=opt.curvature, step_size=opt.step_size, sample_control=opt.sample_control)
     path = rrt.Planning(animation=opt.show_animation)
 
     if path is None:
@@ -341,5 +372,6 @@ if __name__ == '__main__':
     parser.add_argument('--goal_sample_rate', type=float, default=10)
     parser.add_argument('--curvature', type=float, default=1.0)
     parser.add_argument('--step_size', type=float, default=0.1)
+    parser.add_argument('--sample_control', action='store_true')
     opt = parser.parse_args()
     main(opt)
