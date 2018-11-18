a5e7084fcad81642a818bd9e65a6955104505b88
ligongh <hanligong@gmail.com>
2018-11-18 18:04:35 -0500

    lazy

--- problem_two_gazebo.py
+++ problem_two_gazebo.py
@@ -130,12 +130,12 @@ class RRT():
         angle = np.deg2rad(angle)
         duration = random.uniform(0, MAX_DURATION)
         
-        if (currState[0][0]-self.end.x)**2 + (currState[0][1]-self.end.y)**2 < 3.5**2:
-            if random.random() < 1:
-                control = self.calc_control(currState, ([self.end.x, self.end.y, Z_VALUE], 0))
-            else:
-                control = (speed, angle, duration)
-            return control[0], control[1], control[2]
+        # if (currState[0][0]-self.end.x)**2 + (currState[0][1]-self.end.y)**2 < 3.5**2:
+        #     if random.random() < 0.5:
+        #         control = self.calc_control(currState, ([self.end.x, self.end.y, Z_VALUE], 0))
+        #     else:
+        #         control = (speed, angle, duration)
+        #     return control[0], control[1], control[2]
 
         return speed, angle, duration
     
