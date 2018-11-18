2eb3726a1f894f81ed4d6ad11259d8e0ad6b1317
ligongh <hanligong@gmail.com>
2018-11-18 13:47:03 -0500

    lazy

--- problem_two_gazebo.py
+++ problem_two_gazebo.py
@@ -408,8 +408,8 @@ def main(opt):
 
     # Set Initial parameters
     start = [-8., -6., np.deg2rad(90.)]
-    goal = [8., 4., np.deg2rad(0.0)]
-    # goal = [-8, 5, np.deg2rad(90)]
+    # goal = [8., 4., np.deg2rad(0.0)]
+    goal = [-8, 5, np.deg2rad(90)]
 
     agent = Gazebo()
 
