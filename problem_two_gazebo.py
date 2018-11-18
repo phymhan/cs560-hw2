f78886bde360253fd9c279d56f73b516fac78892
ligongh <hanligong@gmail.com>
2018-11-18 18:31:58 -0500

    lazy

--- problem_two_gazebo.py
+++ problem_two_gazebo.py
@@ -444,6 +444,7 @@ class RRT():
         initState = ([self.start.x, self.start.y, Z_VALUE], self.start.yaw)
         self.set_state(initState)
         for control, path in zip(controls, paths[1:]):
+            print(control)
             print('--> action: speed %.1f, angle %.1f, duration %.1f' % (control[0], control[1], control[2]))
             self.agent.action(*control)
             time.sleep(control[2])
