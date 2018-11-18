d13556814aec2e872b992756fd107e1ee2a6eba5
ligongh <hanligong@gmail.com>
2018-11-18 18:24:12 -0500

    lazy

--- problem_two_gazebo.py
+++ problem_two_gazebo.py
@@ -437,16 +437,18 @@ class RRT():
             nodeList.append(node)
         self.nodeList = nodeList
     
-    def replay(self, controls):
+    def replay(self, controls, paths):
         print('===========================================')
         print('REPLAY')
         # set init state
-        initState = ([self.start.x, self.start.y, Z_VALUE], self.start.yaw)
-        self.set_state(initState)
-        for control in controls:
+        prevState = ([self.start.x, self.start.y, Z_VALUE], self.start.yaw)
+        for control in zip(controls, paths[1:]):
+            self.set_state(prevState)
             print('--> action: speed %.1f, angle %.1f, duration %.1f' % (control[0], control[1], control[2]))
             self.agent.action(*control)
             time.sleep(control[2])
+            expState = ([paths[0], paths[1], Z_VALUE], paths[2])
+            self.set_state(expState)
         self.stop()
     
     def stop(self):
@@ -514,7 +516,7 @@ def main(opt):
         plt.show()
 
         # replay
-        rrt.replay(control)
+        rrt.replay(control, path)
 
     if path is None:
         print('Path not found. Maximum # of iterations exceeded.')
